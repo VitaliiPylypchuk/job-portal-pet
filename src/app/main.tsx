4f119fb1373301ed69723df783821d2ea5262acd
Vitaliy Pylypchuk <vitaliypyly@gmail.com>
2026-03-10 21:00:26 +0200

    feat: create enableMocking function so that mocks do not work in PROD mode

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,8 +3,19 @@ import {StrictMode} from "react";
 import {RouterProvider} from "react-router-dom";
 import {router} from "@/app/router.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>
-)
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return
+  }
+
+  const {worker} = await import("@/shared/api/mocks/browser")
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <RouterProvider router={router}/>
+    </StrictMode>
+  )
+})
